3403e9dd7e7daada9db60cfbd1134ce2dc3663b5
Thierry Maignan <maignan.thierry@outlook.fr>
2022-04-27 19:58:47 +0200

    react part 2

--- react-de-zero/src/App.js
+++ react-de-zero/src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import NotFound from "./pages/NotFound"
 import About from "./pages/About"
+import Blog from "./pages/Blog"
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
+          <Route exact path="/blog" element={<Blog />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
