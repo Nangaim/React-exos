b9195635b0570c6103a1201c2c9d0aaf5bfc25e7
Thierry Maignan <maignan.thierry@outlook.fr>
2022-05-05 23:37:44 +0200

    feat: cryp^to-watch

---
+++ crypto-watch/src/components/StarIcon.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react"
+
+const StarIcon = ({ coinId }) => {
+  const [like, setLike] = useState(false)
+  useEffect(() => {
+    if (window.localStorage.coinList) {
+      let favList = window.localStorage.coinList.split(",")
+      if (favList.includes(coinId)) {
+        setLike(true)
+      }
+    }
+  })
+
+  const idCheker = (id) => {
+    let favList = null
+    if (window.localStorage.coinList) {
+      favList = window.localStorage.coinList.split(",")
+    }
+
+    if (favList) {
+      if (favList.includes(id)) {
+        window.localStorage.coinList = favList.filter((coin) => coin !== id)
+        setLike(false)
+      } else {
+        window.localStorage.coinList = [...favList, coinId]
+        setLike(true)
+      }
+    } else {
+      window.localStorage.coinList = coinId
+      setLike(true)
+    }
+  }
+  return (
+    <img
+      src={like ? "./assets/star-full.svg" : "./assets/star-empty.svg"}
+      alt="star-icon"
+      onClick={() => idCheker(coinId)}
+    />
+  )
+}
+
+export default StarIcon
